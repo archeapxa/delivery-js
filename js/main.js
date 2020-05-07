8c51076bd89121a1ec940cb96153193919d9b2ec
archeapxa <60774969+archeapxa@users.noreply.github.com>
2020-05-07 15:37:21 +0300

    lesson 1 end

--- js/main.js
+++ js/main.js
@@ -16,13 +16,71 @@ function toggleModal() {
 const buttonAuth = document.querySelector('.button-auth');
 const closeAuth = document.querySelector('.close-auth');
 const modalAuth = document.querySelector('.modal-auth');
+const logInForm = document.querySelector('#logInForm');
+const loginInput = document.querySelector('#login')
+const userName = document.querySelector('.user-name');
+const buttonOut = document.querySelector('.button-out');
+
+
+let login = localStorage.getItem('gloDelivery');
 
 function toggleModalAuth() {
   modalAuth.classList.toggle('is-open');
 }
 
-buttonAuth.addEventListener('click', toggleModalAuth);
-closeAuth.addEventListener('click', toggleModalAuth);
 
 
-function auto
+function authorized() {
+
+  function logOut() {
+    login = null;
+    localStorage.removeItem('gloDelivery');
+    
+    buttonAuth.style.display = '';
+    userName.style.display = '';
+    buttonOut.style.display = '';
+    buttonOut.removeEventListener('click', logOut);
+    checkAuth();
+  }
+
+  console.log('Authorized');
+
+  userName.textContent = login;
+
+  buttonAuth.style.display = 'none';
+  userName.style.display = 'inline';
+  buttonOut.style.display = 'block';
+
+  buttonOut.addEventListener('click', logOut)
+}
+
+function notAuthorized() {
+  console.log('NOT Authorized');
+
+  function logIn(event) {
+    event.preventDefault();
+    login = loginInput.value;
+
+    localStorage.setItem('gloDelivery', login);
+
+    toggleModalAuth();
+    buttonAuth.removeEventListener('click', toggleModalAuth);
+    closeAuth.removeEventListener('click', toggleModalAuth); 
+    logInForm.removeEventListener('submit', logIn);
+    logInForm.reset();
+    checkAuth();
+  }
+
+  buttonAuth.addEventListener('click', toggleModalAuth);
+  closeAuth.addEventListener('click', toggleModalAuth); 
+  logInForm.addEventListener('submit', logIn)
+} 
+
+function checkAuth() {
+  if (login) {
+    authorized();
+  } else {
+    notAuthorized();
+  }
+}
+checkAuth();
