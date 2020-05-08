a341ee9943a08fb5d464442e4fb293276d081cec
archeapxa <60774969+archeapxa@users.noreply.github.com>
2020-05-08 13:08:57 +0300

    lesson 3

--- js/main.js
+++ js/main.js
@@ -22,15 +22,31 @@ const cardsMenu = document.querySelector('.cards-menu');
 
 let login = localStorage.getItem('gloDelivery');
 
-function toggleModalAuth() {
+
+const getData = async function (url) {
+  
+  const response = await fetch(url);
+
+  if(!response.ok) {
+    throw new Error(`Ошибка по адресу ${url}, статус ошибки ${response.status}!`)
+  }
+
+  return await response.json();
+
+  
+};
+
+
+
+const toggleModalAuth = function() {
   loginInput.style.borderColor = '';
   modalAuth.classList.toggle('is-open');
 
-}
+};
 
-function toggleModal() {
+const toggleModal = function() {
   modal.classList.toggle("is-open");
-}
+};
 
 function authorized() {
 
@@ -92,21 +108,22 @@ function checkAuth() {
 }
 
 // day 2
-function createCardRestaurant() {
+function createCardRestaurant({ image, kitchen, name, price, products, stars, time_of_delivery: timeOfDelivery }) {
+
   const card = `
-    <a class="card card-restaurant">
-      <img src="img/palki-skalki/preview.jpg" alt="image" class="card-image"/>
+    <a class="card card-restaurant" data-products="${products}">
+      <img src="${image}" alt="image" class="card-image"/>
       <div class="card-text">
         <div class="card-heading">
-          <h3 class="card-title">Палки скалки</h3>
-          <span class="card-tag tag">55 мин</span>
+          <h3 class="card-title">${name}</h3>
+          <span class="card-tag tag">${timeOfDelivery} мин</span>
         </div>
         <div class="card-info">
           <div class="rating">
-            4.5
+            ${stars}
           </div>
-          <div class="price">От 500 ₽</div>
-          <div class="category">Пицца</div>
+          <div class="price">${price} ₽</div>
+          <div class="category">${kitchen}</div>
         </div>
       </div>
     </a>
@@ -116,19 +133,19 @@ function createCardRestaurant() {
 
 };
 
-function createCardGood() {
+function createCardGood({ description, id, image, name, price }) {
+
   const card = document.createElement('div');
   card.className = 'card';
 
   card.insertAdjacentHTML('beforeend', `
-    <img src="img/pizza-plus/pizza-classic.jpg" alt="image" class="card-image"/>
+    <img src="${image}" alt="image" class="card-image"/>
     <div class="card-text">
       <div class="card-heading">
-        <h3 class="card-title card-title-reg">Пицца Классика</h3>
+        <h3 class="card-title card-title-reg">${name}</h3>
       </div>
       <div class="card-info">
-        <div class="ingredients">Соус томатный, сыр «Моцарелла», сыр «Пармезан», ветчина, салями,
-          грибы.
+        <div class="ingredients">${description}
         </div>
       </div>
       <div class="card-buttons">
@@ -136,7 +153,7 @@ function createCardGood() {
           <span class="button-card-text">В корзину</span>
           <span class="button-cart-svg"></span>
         </button>
-        <strong class="card-price-bold">510 ₽</strong>
+        <strong class="card-price-bold">${price}</strong>
       </div>
     </div>
   `);
@@ -152,14 +169,17 @@ function openGoods(event) {
 
   if (restaurant) {
     if (login) {
+      
       cardsMenu.textContent = '';
       containerPromo.classList.add('hide');
       restaurants.classList.add('hide');
       menu.classList.remove('hide');
 
-      createCardGood();
-      createCardGood();
-      createCardGood();
+      getData(`./db/${restaurant.dataset.products}`).then(function(data){
+        data.forEach(createCardGood);
+      });
+
+      
     } else {
       toggleModalAuth();
     }
@@ -172,23 +192,28 @@ function returnMain() {
   menu.classList.add('hide')
 }
 
-cartButton.addEventListener("click", toggleModal);
+function init() {
+  getData('./db/partners.json').then(function(data){
+    data.forEach(createCardRestaurant);
+  });
+  
+  cartButton.addEventListener("click", toggleModal);
+  
+  close.addEventListener("click", toggleModal);
+  
+  cardsRestaurants.addEventListener('click', openGoods);
+  
+  logo.addEventListener('click', returnMain);
+  
+  checkAuth();
+  
+  
+  new Swiper('.swiper-container', {
+    loop: true,
+    autoplay: {
+      delay: 3000,
+    }
+  }); 
+}
 
-close.addEventListener("click", toggleModal);
-
-cardsRestaurants.addEventListener('click', openGoods);
-
-logo.addEventListener('click', returnMain);
-
-checkAuth();
-
-createCardRestaurant();
-createCardRestaurant();
-createCardRestaurant();
-
-new Swiper('.swiper-container', {
-  loop: true,
-  autoplay: {
-    delay: 5000,
-  }
-}); 
+init();
